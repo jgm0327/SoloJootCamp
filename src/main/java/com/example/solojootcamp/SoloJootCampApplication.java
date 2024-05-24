f415a7a81573c63e5e9ec6d11193591b9cf734c6
jgm0327 <gmjang6349@gmail.com>
2024-05-24 17:20:18 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/example/solojootcamp/SoloJootCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.solojootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloJootCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloJootCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solojootcamp/SoloJootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solojootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloJootCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
