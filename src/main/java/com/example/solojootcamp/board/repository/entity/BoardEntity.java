5196a041a667b7065a1822580cf12771459ac89f
jgm0327 <gmjang6349@gmail.com>
2024-05-24 18:43:11 +0900

    #1 - 게시판 생성을 위한 빌더 생성

--- src/main/java/com/example/solojootcamp/board/repository/entity/BoardEntity.java
+++ src/main/java/com/example/solojootcamp/board/repository/entity/BoardEntity.java
@@ -1,13 +1,13 @@
 package com.example.solojootcamp.board.repository.entity;
 
-import com.example.solojootcamp.board.service.dto.Board;
 import com.example.solojootcamp.common.base.BaseModifyEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Getter
 @Table(name = "board")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardEntity extends BaseModifyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +17,9 @@ public class BoardEntity extends BaseModifyEntity {
 
     private String description;
 
+    @Builder
+    public BoardEntity(String title, String description){
+        this.title = title;
+        this.description = description;
+    }
 }
