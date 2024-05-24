a90789565cca6745f46a7fa3b2a2249ef17e2e32
jgm0327 <gmjang6349@gmail.com>
2024-05-24 18:58:22 +0900

    #1 - 게시판 삭제를 위한 매핑 추가

--- src/main/java/com/example/solojootcamp/board/controller/BoardApiController.java
+++ src/main/java/com/example/solojootcamp/board/controller/BoardApiController.java
@@ -2,7 +2,6 @@ package com.example.solojootcamp.board.controller;
 
 import com.example.solojootcamp.board.controller.dto.BoardResponse;
 import com.example.solojootcamp.board.controller.dto.UpsertRequest;
-import com.example.solojootcamp.board.repository.BoardRepository;
 import com.example.solojootcamp.board.service.BoardService;
 import com.example.solojootcamp.board.service.dto.UpsertBoard;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequestMapping("/boards")
 @RestController
@@ -18,8 +16,9 @@ import java.util.stream.Collectors;
 public class BoardApiController {
 
     private final BoardService boardService;
+
     @GetMapping("")
-    public ResponseEntity<List<BoardResponse>> getBoards(){
+    public ResponseEntity<List<BoardResponse>> getBoards() {
         List<BoardResponse> boards = boardService.getBoards()
                 .stream().map(BoardResponse::from).toList();
 
@@ -27,21 +26,27 @@ public class BoardApiController {
     }
 
     @GetMapping("/{boardId}")
-    public ResponseEntity<BoardResponse> getBoard(@PathVariable("boardId") long id){
+    public ResponseEntity<BoardResponse> getBoard(@PathVariable("boardId") long id) {
         BoardResponse board = BoardResponse.from(boardService.getBoard(id));
 
         return ResponseEntity.ok(board);
     }
 
     @PostMapping("")
-    public ResponseEntity<Void> addBoard(@RequestBody UpsertRequest upsertRequest){
+    public ResponseEntity<Void> addBoard(@RequestBody UpsertRequest upsertRequest) {
         boardService.addBoard(UpsertBoard.from(upsertRequest));
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{boardId}")
-    public ResponseEntity<Void> modifyBoard(@RequestBody UpsertRequest upsertRequest, @PathVariable("boardId") long id){
+    public ResponseEntity<Void> modifyBoard(@RequestBody UpsertRequest upsertRequest, @PathVariable("boardId") long id) {
         boardService.modifyBoard(UpsertBoard.from(upsertRequest), id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{boardId}")
+    public ResponseEntity<Void> deleteBoard(@PathVariable("boardId") long id) {
+        boardService.deleteBoard(id);
         return ResponseEntity.ok().build();
     }
 }
